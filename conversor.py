94982acaac825fae43598e7808e8a134496fd35e
lizeth309 <124295237+lizeth309@users.noreply.github.com>
2023-03-02 12:55:36 -0600

    Add files via upload

---
+++ conversor.py
@@ -0,0 +1,13 @@
+
+
+from tkinter import *
+from tkinter import ttk
+
+class conversor:
+    #tipo constructor de la clase
+    def __init__(self, raiz):
+          raiz.title("Pies a metros")
+
+if __name__=="__main__":
+    print("este es el archivo printipal.")
+    print("nada mas se mostrara esto si es el archivo prinipal")
